0ecebc570735e27dce88c13dc5e4a61e9ab106a7
RAHUL S KUMAR <rsk9946@gmail.com>
2020-09-23 11:36:14 +0530

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFY3VE-Lab66r87vyUcqr0b5-VsmHntek",
-  authDomain: "amazncloneweb.firebaseapp.com",
-  databaseURL: "https://amazncloneweb.firebaseio.com",
-  projectId: "amazncloneweb",
-  storageBucket: "amazncloneweb.appspot.com",
-  messagingSenderId: "601804193460",
-  appId: "1:601804193460:web:c72a0a8479cffb6f0261e1",
-  measurementId: "G-GCGFCEFGTZ"
+ 
 };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
